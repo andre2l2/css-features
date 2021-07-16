85044e7ac9e16cf0bed0fc5b8f2392f05e37f564
Andre Ferreira <delcferreira@gmail.com>
2021-07-15 23:20:25 -0300

    add new pages 2

--- scroll.js
+++ scroll.js
@@ -8,7 +8,6 @@ let next = 0;
 body.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowRight' && next < totalOfTheSections) {
         next++;
-        console.log(percent(totalOfTheSections, next));
         progessBar.style.width = `${percent(totalOfTheSections, next)}%`;
     };
     if (e.key === 'ArrowLeft' && next > 0) {
@@ -37,5 +36,9 @@ function changeAvatar() {
         }
     })
 };
+
+const style = 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)';
+console.log('%c Dev!', style);
+
 
 changeAvatar();
